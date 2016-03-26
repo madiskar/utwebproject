4c76db5e645ac04b74fd9c2389d1c7672f59e927
Madis Kariler <madis.kariler@gmail.com>
2016-03-27 00:23:05 +0200

    Update browsing_english.js

--- public_html/public/js/browsing_english.js
+++ public_html/public/js/browsing_english.js
@@ -3,7 +3,6 @@
 window.onload = checkHash();
 
 window.addEventListener('hashchange', function() {
-	console.log("hash has changed!");
 	checkHash();
 });
 
@@ -19,44 +18,44 @@ function buildHTML(gamesArray) {
 	
 	for(var i = 0; i<gamesArray.length; i++) {
     	
-	    var gameLink = document.createElement("A"); //anchor
-	    gameLink.id = "gamelink";
-	    gameLink.href = base_url_origin + "/wasdreviews/public_html/index.php/games/" + gamesArray[i][3];
-	    //gameLink.href = "http://wasdreviews.cs.ut.ee/index.php/games/" + gamesArray[i][3];
-	    
-	    var gameContainer = document.createElement("div"); //maincontainer
-	    gameContainer.id = "container";
-	    gameContainer.className = "gameContainer";
-	    
-	   	var title = document.createElement("div"); //title
-	   	title.id = "titleid";
-	    title.className = "gameTitle";
-    	title.innerHTML = gamesArray[i][0];
-    	
-    	var gameDesc = document.createElement("div"); //description
-    	gameDesc.id = "gamedesc";
-    	gameDesc.className = "gameDescription";
-    	gameDesc.innerHTML = gamesArray[i][1];
-
-    	var avgRating = document.createElement("div"); // average rating
-    	avgRating.id = "avgrating";
-    	avgRating.className = "gameRating";
-    	avgRating.innerHTML = "Rating: " + gamesArray[i][2];
-
-    	var thumbnail = document.createElement("IMG"); // thumbnail
-    	thumbnail.className = "gameImage";
-    	thumbnail.alt = gamesArray[i][0]  + " screenshot";
-    	thumbnail.src = base_url_origin + "/wasdreviews/public_html/public/images/TEMP_thumbnail.png";
-    	//thumbnail.src = "http://wasdreviews.cs.ut.ee/public/images/" + gamesArray[i][3] + "/" + gamesArray[i][4];
-    	var lineBreak = document.createElement("BR"); // line break
-    	
-    	gameContainer.appendChild(thumbnail);
-    	gameContainer.appendChild(title);
-    	gameContainer.appendChild(lineBreak);
-    	gameContainer.appendChild(gameDesc);
-    	gameContainer.appendChild(avgRating);
-    	gameLink.appendChild(gameContainer);
-    	document.getElementById("games").appendChild(gameLink);
+		var gameLink = document.createElement("A"); //anchor
+		gameLink.id = "gamelink";
+		gameLink.href = base_url_origin + "/wasdreviews/public_html/index.php/games/" + gamesArray[i].slug;
+		//gameLink.href = "http://wasdreviews.cs.ut.ee/index.php/games/" + gamesArray[i].slug;
+		    
+		var gameContainer = document.createElement("div"); //maincontainer
+		gameContainer.id = "container";
+		gameContainer.className = "gameContainer";
+		    
+		var title = document.createElement("div"); //title
+		title.id = "titleid";
+		title.className = "gameTitle";
+	    	title.innerHTML = gamesArray[i].title;
+	    	
+	    	var gameDesc = document.createElement("div"); //description
+	    	gameDesc.id = "gamedesc";
+	    	gameDesc.className = "gameDescription";
+	    	gameDesc.innerHTML = gamesArray[i].description;
+	
+	    	var avgRating = document.createElement("div"); // average rating
+	    	avgRating.id = "avgrating";
+	    	avgRating.className = "gameRating";
+	    	avgRating.innerHTML = "Rating: " + gamesArray[i].average_rating;
+	
+	    	var thumbnail = document.createElement("IMG"); // thumbnail
+	    	thumbnail.className = "gameImage";
+	    	thumbnail.alt = gamesArray[i][0]  + " screenshot";
+	    	thumbnail.src = base_url_origin + "/wasdreviews/public_html/public/images/TEMP_thumbnail.png";
+	    	//thumbnail.src = "http://wasdreviews.cs.ut.ee/public/images/" + gamesArray[i].slug + "/" + gamesArray[i].thmb_extension;
+	    	var lineBreak = document.createElement("BR"); // line break
+	    	
+	    	gameContainer.appendChild(thumbnail);
+	    	gameContainer.appendChild(title);
+	    	gameContainer.appendChild(lineBreak);
+	    	gameContainer.appendChild(gameDesc);
+	    	gameContainer.appendChild(avgRating);
+	    	gameLink.appendChild(gameContainer);
+	    	document.getElementById("games").appendChild(gameLink);
 	}
 }
 
@@ -73,8 +72,8 @@ function handleClick() {
 	var genreType = genreValue.options[genreValue.selectedIndex].value;
 
 	var currentSelection = [sortBy, genreType];
-	history.pushState(currentSelection, null, "#?sortby=" + sortBy + "&genre=" + genreType);
 	displaySelection(sortBy, genreType);
+	history.pushState(currentSelection, null, "#?sortby=" + sortBy + "&genre=" + genreType);
 }
 
 function setSelectedItems(sortby, genre) {

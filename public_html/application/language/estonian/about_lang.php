f5ac4e9ca2a30d8384aa7ca6471dce87b8f9a7d4
Madis Kariler <madis.kariler@gmail.com>
2016-04-09 14:48:20 +0300

    Added files via upload

---
+++ public_html/application/language/estonian/about_lang.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['gamecount'] = 'Arvustatud mängude arv';
+$lang['usercount'] = 'Registreeritud kasutajate arv';
+$lang['reviewcount'] = 'Kasutajaarvustuste arv';

63c30de95c9511247893d3e4fec15c51f4444812
Madis Kariler <madis.kariler@gmail.com>
2016-04-09 14:47:51 +0300

    Added files via upload

---
+++ public_html/application/language/english/about_lang.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['gamecount'] = 'Number of games reviewed';
+$lang['usercount'] = 'Number of users registered';
+$lang['reviewcount'] = 'Number of user reviews submitted';

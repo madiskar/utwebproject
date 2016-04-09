6f3765261e98494c83d2fd7d558796c2d3c56ab7
Madis Kariler <madis.kariler@gmail.com>
2016-04-09 21:00:46 +0300

    Update about_lang.php

--- public_html/application/language/estonian/about_lang.php
+++ public_html/application/language/estonian/about_lang.php
@@ -1,6 +1,20 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['gamecount'] = 'Arvustatud mängude arv';
+$lang['mapstuff'] = 'Kus me asume?';
+$lang['statstuff'] = 'Statistikat meie veebilehe kohta';
+$lang['gamecount'] = 'Arvustatud mängude koguarv';
+$lang['gamecount1'] = 'Mängude arv "action" žanris';
+$lang['gamecount2'] = 'Mängude arv "seiklus" žanris';
+$lang['gamecount3'] = 'Mängude arv "casual" žanris';
+$lang['gamecount4'] = 'Mängude arv "indie" žanris';
+$lang['gamecount5'] = 'Mängude arv "MMO" žanris';
+$lang['gamecount6'] = 'Mängude arv "võidusõit" žanris';
+$lang['gamecount7'] = 'Mängude arv "RPG" žanris';
+$lang['gamecount8'] = 'Mängude arv "simulatsioon" žanris';
+$lang['gamecount9'] = 'Mängude arv "sport" žanris';
+$lang['gamecount10'] = 'Mängude arv "strateegia" žanris';
 $lang['usercount'] = 'Registreeritud kasutajate arv';
 $lang['reviewcount'] = 'Kasutajaarvustuste arv';
+$lang['mostactive'] = 'Kõige aktiivsem kasutaja';
+$lang['reviews'] = 'arvustust';

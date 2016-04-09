0426f25ac665678ae3f7d2a85a608c0970f1eca8
Madis Kariler <madis.kariler@gmail.com>
2016-04-09 21:06:21 +0300

    Update view_stats.php

--- public_html/application/views/about/view_stats.php
+++ public_html/application/views/about/view_stats.php
@@ -1,13 +1,52 @@
 <div class="mainContainer">
+<hr>
+<div class="headLineItem"><h3> <?php echo $statstuff; ?> </h3></div>
 <?php $xml = simplexml_load_file('/webpages/wasdreviewscsut/public_html/public/xml/stats.xml'); ?>
 
 	<div class="statItem">
-		<?php echo $gamecount . " - " . $xml->gamecount; ?>
+		<?php echo $gamecount . " - " . $xml->genrecount->total_count; ?>
+	</div>	
+	<div class="substatItem">
+		<?php echo $gamecount1 . " - " . $xml->genrecount->action_count; ?>
 	</div>
+	<div class="substatItem">
+		<?php echo $gamecount2 . " - " . $xml->genrecount->adventure_count; ?>
+	</div>
+	<div class="substatItem">
+		<?php echo $gamecount3 . " - " . $xml->genrecount->casual_count; ?>
+	</div>
+	<div class="substatItem">
+		<?php echo $gamecount4 . " - " . $xml->genrecount->indie_count; ?>
+	</div>
+	<div class="substatItem">
+		<?php echo $gamecount5 . " - " . $xml->genrecount->mmo_count; ?>
+	</div>
+	<div class="substatItem">
+		<?php echo $gamecount6 . " - " . $xml->genrecount->racing_count; ?>
+	</div>
+	<div class="substatItem">
+		<?php echo $gamecount7 . " - " . $xml->genrecount->rpg_count; ?>
+	</div>
+	<div class="substatItem">
+		<?php echo $gamecount8 . " - " . $xml->genrecount->simulation_count; ?>
+	</div>
+	<div class="substatItem">
+		<?php echo $gamecount9 . " - " . $xml->genrecount->sports_count; ?>
+	</div>
+	<div class="substatItem">
+		<?php echo $gamecount10 . " - " . $xml->genrecount->strategy_count; ?>
+	</div>
+	
+		
 	<div class="statItem">
 		<?php echo $usercount . " - " . $xml->usercount; ?>
 	</div>
 	<div class="statItem">
 		<?php echo $reviewcount . " - " . $xml->reviewcount; ?>
 	</div>
+	<div class="statItem">
+		<?php echo $mostactive . " - " . $xml->mostactive->username . " (" . $xml->mostactive->userreviewcount . " " . $howmany . ")"; ?>
+	</div>
+	<br>
+	<hr>
 </div>

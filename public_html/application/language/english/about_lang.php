ad37e8aec582435852e5078f8599af7bbe2fe8f2
Madis Kariler <madis.kariler@gmail.com>
2016-04-09 20:59:03 +0300

    Update about_lang.php

--- public_html/application/language/english/about_lang.php
+++ public_html/application/language/english/about_lang.php
@@ -1,6 +1,20 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['gamecount'] = 'Number of games reviewed';
+$lang['mapstuff'] = 'Where are we?';
+$lang['statstuff'] = 'Some stats about our website';
+$lang['gamecount'] = 'Total number of games reviewed';
+$lang['gamecount1'] = 'Number of games in "action" genre';
+$lang['gamecount2'] = 'Number of games in "adventure" genre';
+$lang['gamecount3'] = 'Number of games in "casual" genre';
+$lang['gamecount4'] = 'Number of games in "indie" genre';
+$lang['gamecount5'] = 'Number of games in "MMO" genre';
+$lang['gamecount6'] = 'Number of games in "racing" genre';
+$lang['gamecount7'] = 'Number of games in "RPG" genre';
+$lang['gamecount8'] = 'Number of games in "simulation" genre';
+$lang['gamecount9'] = 'Number of games in "sports" genre';
+$lang['gamecount10'] = 'Number of games in "strategy" genre';
 $lang['usercount'] = 'Number of users registered';
 $lang['reviewcount'] = 'Number of user reviews submitted';
+$lang['mostactive'] = 'Most active user';
+$lang['reviews'] = 'reviews';

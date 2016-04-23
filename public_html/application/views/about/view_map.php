e4619eed20774240183cb360f139d769adf04cb8
Madis Kariler <madis.kariler@gmail.com>
2016-04-23 21:12:42 +0300

    Update view_map.php

--- public_html/application/views/about/view_map.php
+++ public_html/application/views/about/view_map.php
@@ -1,10 +1,15 @@
-<div class="mapItem">
-	<hr>
+<div class="breakItem">
+		<hr>
+	</div>
+<div class="headLineItem"><h3> <?php echo $mapstuff; ?> </h3></div>	
 	<br>
-	<div class="headLineItem"><h3> <?php echo $mapstuff; ?> </h3></div>
-	<?php echo $map['js']; ?>
-	<?php echo $map['html'];?>
+	<div id="map-div" class="mapItem">
+		<script defer src="http://wasdreviews.cs.ut.ee/public/js/googlemapstuff.js"></script>
+	</div>
+	
 	<br>
 	<br>
-	<hr>
+	<div class="breakItem">
+		<hr>
+	</div>
 </div>
